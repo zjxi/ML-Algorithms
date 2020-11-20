29ae643ea0a1ab7006562793799d11642c6ffb78
zjxi <hsi0090@163.com>
2020-11-20 19:05:54 +0800

    更新第9章EM算法

--- 第9章 EM算法及其推广/EM.py
+++ 第9章 EM算法及其推广/EM.py
@@ -3,60 +3,78 @@
     written by zjxi @ 2020/11/14
 """
 
+from scipy import stats
 import numpy as np
 
+# 硬币投掷结果观测序列
+observations = np.array([[1, 0, 0, 0, 1, 1, 0, 1, 0, 1],
+                         [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+                         [1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+                         [1, 0, 1, 0, 0, 0, 1, 1, 0, 0],
+                         [0, 1, 1, 1, 0, 1, 1, 1, 0, 1]])
 
-def load_data(filename):
+
+def em_single(priors, observations):
     """
-    导入训练集或者测试集数据
-    :param filename: 数据文件路径
-    :return: 特征向量，标签向量
+    EM算法单次迭代
+    Arguments
+    ---------
+    priors : [theta_A, theta_B]
+    observations : [m X n matrix]
+
+    Returns
+    --------
+    new_priors: [new_theta_A, new_theta_B]
+    :param priors:
+    :param observations:
+    :return:
     """
-    feat, label = [], []
-    with open(filename, 'r') as fr:
-        for line in fr.readlines():
-            line = line.strip('\n').split(',')
-            # 添加标签向量
-            label.append(int(line[-1]))
-            # 添加特征向量
-            feat.append([float(e) for e in line[:-1]])
-    
-    return feat, label
+    counts = {'A': {'H': 0, 'T': 0}, 'B': {'H': 0, 'T': 0}}
+    theta_A = priors[0]
+    theta_B = priors[1]
+    # E step
+    for observation in observations:
+        len_observation = len(observation)
+        num_heads = observation.sum()
+        num_tails = len_observation - num_heads
+        contribution_A = stats.binom.pmf(num_heads, len_observation, theta_A)
+        contribution_B = stats.binom.pmf(num_heads, len_observation, theta_B)  # 两个二项分布
+        weight_A = contribution_A / (contribution_A + contribution_B)
+        weight_B = contribution_B / (contribution_A + contribution_B)
+        # 更新在当前参数下A、B硬币产生的正反面次数
+        counts['A']['H'] += weight_A * num_heads
+        counts['A']['T'] += weight_A * num_tails
+        counts['B']['H'] += weight_B * num_heads
+        counts['B']['T'] += weight_B * num_tails
+    # M step
+    new_theta_A = counts['A']['H'] / (counts['A']['H'] + counts['A']['T'])
+    new_theta_B = counts['B']['H'] / (counts['B']['H'] + counts['B']['T'])
+    return [new_theta_A, new_theta_B]
 
 
-class EM:
-    def __init__(self, trainData, trainLbl, testData, testLbl, epoch):
-        """
-        初始化逻辑回归的各类参数
-        :param trainData: 训练特征向量集
-        :param trainLbl: 训练标签向量集
-        :param testData: 测试特征向量集
-        :param testLbl: 测试标签向量集
-        :param epoch: 训练迭代轮数
-        """
-        self.train_data = trainData
-        self.train_label = trainLbl
-        self.test_data = testData
-        self.test_label = testLbl
-        self.epoch = epoch
-
-    def training(self):
-        pass
-
-    def predict(self):
-        pass
-
-    def test(self):
-        pass
+def em(observations, prior, tol=1e-6, iterations=10000):
+    """
+    EM算法
+    :param observations: 观测数据
+    :param prior: 模型初值
+    :param tol: 迭代结束阈值
+    :param iterations: 最大迭代次数
+    :return: 局部最优的模型参数
+    """
+    import math
+    iteration = 0
+    while iteration < iterations:
+        new_prior = em_single(prior, observations)
+        delta_change = np.abs(prior[0] - new_prior[0])
+        if delta_change < tol:
+            break
+        else:
+            prior = new_prior
+            iteration += 1
+    return [new_prior, iteration]
 
 
-if __name__ == '__main__':
-    # # 读取训练数据集
-    train_data, train_lbl = load_data("../data/train.txt")
-    # 读取测试数据集
-    test_data, test_lbl = load_data("../data/test.txt")
-
-    # EM算法流程
-    em = EM(train_data, train_lbl, test_data, test_lbl,
-            epoch=50)
+if __name__ == "__main__":
+    result = em(observations, [0.6, 0.4])
+    print(result)
 
