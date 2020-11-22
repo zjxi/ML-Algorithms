9f32fd7fd93d1b9a9df5f55c1d161f81b1fb11c7
zjxi <hsi0090@163.com>
2020-11-22 21:24:14 +0800

    更新第14章层次聚类算法

--- 第14章 聚类方法/hierarchical_clustering.py
+++ 第14章 聚类方法/hierarchical_clustering.py
@@ -23,9 +23,119 @@ def load_data(filename):
 
 
 class HierarchicalClustering:
-    def __init__(self):
+    def __init__(self, trainData):
+        """
+        初始化逻辑回归的各类参数
+        :param trainData: 训练特征向量集
+        """
+        self.train_data = trainData
+
+    @staticmethod
+    def calc_euclidean_distance(xi, xj):
+        """
+        计算两个不同样本之间的欧氏距离
+        :param xi: 样本xi
+        :param xj: 样本xj
+        :return: xi、xj的欧氏距离
+        """
+        return np.square(np.sum((xi - xj) * (xi - xj)))
+
+    @staticmethod
+    def calc_manhattan_distance(xi, xj):
+        """
+        计算两个不同样本之间的曼哈顿距离
+        :param xi: 样本xi
+        :param xj: 样本xj
+        :return: xi、xj的曼哈顿距离
+        """
+        return np.sum(np.abs(xi - xj))
+
+    @staticmethod
+    def calc_chebyshev_distance(xi, xj):
+        """
+        计算两个不同样本之间的切比雪夫距离
+        :param xi: 样本xi
+        :param xj: 样本xj
+        :return: xi、xj的切比雪夫距离
+        """
+        return max(np.abs(xi - xj))
+
+    def calc_single_linkage(self, G_p, G_q):
         pass
+
+    def clustering(self, cls_num):
+        """
+        算法14.1 聚合聚类算法
+        :param cls_num: 需要聚类的个数
+        :return: 最终的聚类
+        """
+        row, col = np.shape(self.train_data)
+        # (1) 计算n个样本两两之间的欧氏距离{dij}, 记作矩阵D = [dij]nxn
+        # 在这里 矩阵D 等价于 D_ij
+        D_ij = np.zeros((row, col))
+        for i in range(row):
+            for j in range(row):
+                if i != j:
+                    D_ij[i][j] = self.calc_euclidean_distance(self.train_data[i], self.train_data[j])
+
+        # (2) 构造n个类, 每个类只包含一个样本
+        G_i = []
+        # 首先根据row个样本构建row个类, 即Gi = {xi}, i=1, 2, ... ,row
+        # 为了方便计算这里只需要存储每个样本的序号即可
+        # i为样本的序号，true代表当前样本没有被合并，False代表被合并为新类
+        cnt = row
+        for i in range(row):
+            G_i.append([i])
+
+        while 1:
+            # (3) 合并类间距离最小的两个类，其中最短距离为类间距离，构建一个新类
+            # 声明任意两个类之间的最短距离min_dis
+            # i_idx, j_idx 分别为 最短距离的样本i、样本j的下标
+            min_dis = D_ij[0][1]
+            i_idx, j_idx = 0, 1
+            for i in range(row):
+                for j in range(row):
+                    if i != j:
+                        if D_ij[i][j] < min_dis:
+                            min_dis = D_ij[i][j]
+                            i_idx, j_idx = i, j
+
+            # 合并成新类，类的下标加1
+            G_i.append([i_idx, j_idx])
+            cnt += 1
+            # 然后将被合并的两个样本的列表分别清空
+            G_i[i_idx].clear()
+            G_i[j_idx].clear()
+
+            # (4) 计算新类与当前各类的距离。若类的个数为1，终止计算，否则回到步(3)
+            for i in G_i[cnt-1]:
+                for lis in G_i:
+                    if len(lis) != 0:
+                        for j in lis:
+                            if D_ij[i][j] < min_dis:
+                                min_dis = D_ij[i][j]
+                                i_idx, j_idx = i, j
+
+
+
+
+            # 计算当前类的个数
+            cls_cur = 0
+            for i in range(len(G_i)):
+                if len(G_i[i]) != 0:
+                    cls_cur += 1
+            # 若当前类的个数为需要聚类的个数，则终止循环，聚类结束
+            if cls_cur == cls_num:
+                break
+
+        # 返回最终的聚类
+        return G_i[len(G_i)-1]
 
 
 if __name__ == '__main__':
-    pass
+    # 读取训练数据集
+    train_data = load_data("../data/train.txt")
+    # 层次聚合聚类算法
+    hc = HierarchicalClustering(train_data)
+    cls = hc.clustering(3)
+    print(cls)
